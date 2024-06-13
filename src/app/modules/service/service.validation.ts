4d0d6b054c0849f6dbeb318d63116dcd7cee95d6
habib-153 <h.r.sihab155@gmail.com>
2024-06-13 22:05:09 +0600

    service interface, model and validation

---
+++ src/app/modules/service/service.validation.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+const createServiceValidationSchema = z.object({
+    body: z.object({
+        name: z.string(),
+        description: z.string(),
+        price: z.number(),
+        duration: z.number(),
+        isDeleted: z.boolean().optional()
+    })
+})
+
+const updateServiceValidationSchema = z.object({
+    body: z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        price: z.number().optional(),
+        duration: z.number().optional(),
+        isDeleted: z.boolean().optional()
+    })
+})
+
+export const ServiceValidation = {
+    createServiceValidationSchema,
+    updateServiceValidationSchema
+}
