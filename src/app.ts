267b4362be691d5f424050107e19ac6dfef9ddf7
habib-153 <h.r.sihab155@gmail.com>
2024-08-30 10:35:15 +0600

    chore: Update CORS origin to include production URL

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 //const port = 3000
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true}))
+app.use(cors({ origin: ['http://localhost:5173', 'https://shine-slot-client.vercel.app'], credentials: true}))
 
 //  application routes
 app.use('/api', router)
